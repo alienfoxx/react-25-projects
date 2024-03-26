3045875e84498cd30b8f89672463aba620c88ecf
alienfoxx <alienfoxx001@gmail.com>
2024-03-26 03:06:24 +0000

    finsh the accordinion project

---
+++ react-basic-concepts/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Accordian from './component/accordian';
+
+function App() {
+  return (
+    <div className="App">
+      {/* Accordian Component */}
+      <Accordian />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-basic-concepts/src/component/accordian/data.js
@@ -0,0 +1,29 @@
+const data = [
+    {
+      id: 1,
+      question: "What is the capital of France?",
+      answer: "Paris",
+    },
+    {
+      id: 2,
+      question: "Who painted the Mona Lisa?",
+      answer: "Leonardo da Vinci",
+    },
+    {
+      id: 3,
+      question: "What is the largest planet in our solar system?",
+      answer: "Jupiter",
+    },
+    {
+      id: 4,
+      question: "What is the symbol for the chemical element iron?",
+      answer: "Fe",
+    },
+    {
+      id: 5,
+      question: "Who wrote the play 'Hamlet'?",
+      answer: "William Shakespeare",
+    },
+    
+  ];
+  export default data;
