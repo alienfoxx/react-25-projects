8652c91244c404a3a3fb9f35cf5eee5dab3fb235
alienfoxx <alienfoxx001@gmail.com>
2024-03-27 01:55:56 +0000

    finshinig the Color picker & stars rating componenets

--- react-basic-concepts/src/App.js
+++ react-basic-concepts/src/App.js
@@ -1,12 +1,17 @@
-
-import './App.css';
-import Accordian from './component/accordian';
+import "./App.css";
+import Accordian from "./component/accordian";
+import RandomColor from "./component/random-color";
+import StarRating from "./component/star-rating";
 
 function App() {
   return (
     <div className="App">
       {/* Accordian Component */}
-      <Accordian />
+      {/* <Accordian /> */}
+      {/* Random Color Component */}
+      {/* <RandomColor /> */}
+      {/* Star Rating Component */}
+      <StarRating/>
     </div>
   );
 }
---
+++ react-basic-concepts/src/component/star-rating/index.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import { FaStar } from "react-icons/fa";
+import "./styles.css";
+
+export default function StarRating({ numOfStars = 7 }) {
+  const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
+  //this function is for when we click get the index of the stars
+  function handleClick(getCurrentIndex) {
+    setRating(getCurrentIndex);
+  }
+  // this one for getting the idex when we hover
+  function handleMouseEnter(getCurrentIndex) {
+    setHover(getCurrentIndex);
+  }
+  function handleMouseLeave() {
+    setHover(rating);
+  }
+
+  return (
+    <div className="star-rating">
+      {[...Array(numOfStars)].map((_, index) => {
+        index += 1;
+        return (
+          <FaStar
+            key={index}
+            className={index <= (hover || rating) ? "active" : "inactive"}
+            onClick={() => handleClick(index)}
+            onMouseMove={() => handleMouseEnter(index)}
+            onMouseLeave={() => handleMouseLeave()}
+            size={40}
+          />
+        );
+      })}
+    </div>
+  );
+}
