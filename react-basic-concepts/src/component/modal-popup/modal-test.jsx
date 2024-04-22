986586e7c4a9a222a7be216b56cb5e4552e3a138
alienfoxx <alienfoxx001@gmail.com>
2024-04-22 01:46:41 +0100

    finshing thePopup  componenent/All Is Done

--- react-basic-concepts/src/component/modal-popup/modal-test.jsx
+++ react-basic-concepts/src/component/modal-popup/modal-test.jsx
@@ -1,17 +1,29 @@
-import { useState } from "react"
+import { useState } from "react";
 import Modal from "./modal";
+import "./modal.css";
 
-export default function ModalTest(){
-    const [showModalPopup,setShowModalPopup]=useState(false);
+export default function ModalTest() {
+  const [showModalPopup, setShowModalPopup] = useState(false);
 
-    function handalPopupToggel(){
-        setShowModalPopup(!showModalPopup)
-    }
+  function handalPopupToggel() {
+    setShowModalPopup(!showModalPopup);
+  }
+  function onClose() {
+    setShowModalPopup(false);
+  }
 
-    return <div>
-        <button onClick={handalPopupToggel}>Open Pop Up</button>
-        {
-            showModalPopup  && <Modal/>
-        }
+  return (
+    <div>
+      <button onClick={handalPopupToggel}>Open Pop Up</button>
+      {showModalPopup && (
+        <Modal
+          id={"custom-id"}
+          header={<h1>Custome Header</h1>}
+          onClose={onClose}
+          body={<div>Customise body</div>}
+          footer={<h1>Customized Footer</h1>}
+        />
+      )}
     </div>
+  );
 }
--- react-basic-concepts/src/component/modal-popup/modal.jsx
+++ react-basic-concepts/src/component/modal-popup/modal.jsx
@@ -1,24 +1,24 @@
-export default function Modal({ id,header,body,footer}) {
+export default function Modal({ id, header, body, footer, onClose }) {
   return (
     <div id={id || "Modal"} className="modal">
       <div className="modal-content">
         <div className="header">
-            <span className="close-modal-icon">&times;</span>
-            <h2>{header?header:'Header'}</h2>
+          <span onClick={onClose} className="close-modal-icon">
+            &times;
+          </span>
+          <h2>{header ? header : "Header"}</h2>
         </div>
         <div className="body">
-            {body?(body):(
-                <div>
-                    <p>This is our bodey Modal</p>
-                </div>
-            )}
+          {body ? (
+            body
+          ) : (
+            <div>
+              <p>This is our bodey Modal</p>
+            </div>
+          )}
         </div>
         <div className="footer">
-        {footer?(footer):(
-                <div>
-                    <h3>This is our Footer Modal</h3>
-                </div>
-            )}
+          {footer ? footer : <h3>This is our Footer Modal</h3>}
         </div>
       </div>
     </div>
