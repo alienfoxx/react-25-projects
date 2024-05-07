1de1936ebd3823a989d6302024e42563b01e099f
alienfoxx <alienfoxx001@gmail.com>
2024-05-08 00:20:30 +0100

    fixing the data show prob

--- react-basic-concepts/src/component/feauture-flag/context/index.jsx
+++ react-basic-concepts/src/component/feauture-flag/context/index.jsx
@@ -1,29 +1,31 @@
 import { createContext, useEffect, useState } from "react";
+import featureFlagsDataServiceCall from "../data";
 
 export const FeatureFlagContext = createContext(null);
 
 export default function FeatureFlagGlobalState({ children }) {
-    const [loading,setLoading] = useState(false);
-    const [enableFlags,setEnabelFlags] = useState(null)
+  const [loading, setLoading] = useState(false);
+  const [enableFlags, setEnabelFlags] = useState(null);
 
-    async function fetchFeatureFlags(){
-        try{
-           
-        }catch(error){
-            console.log(error);
-            throw new Error(error)
-        }
+  async function fetchFeatureFlags() {
+    try {
+      setLoading(true);
+      const response = await featureFlagsDataServiceCall();
+
+      setEnabelFlags(response);
+      setLoading(false)
+    } catch (error) {
+      console.log(error);
+      throw new Error(error);
+      setLoading(false)
     }
+  }
 
-
-
-    useEffect(()=>{
-
-        fetchFeatureFlags();
-
-    },[])
+  useEffect(() => {
+    fetchFeatureFlags();
+  }, []);
   return (
-    <FeatureFlagContext.Provider value={{}}>
+    <FeatureFlagContext.Provider value={{ loading,enableFlags }}>
       {children}
     </FeatureFlagContext.Provider>
   );
--- react-basic-concepts/src/component/feauture-flag/index.jsx
+++ react-basic-concepts/src/component/feauture-flag/index.jsx
@@ -1,8 +1,49 @@
+import { useContext } from "react";
+import Accordian from "../accordian";
+import LightDarkMode from "../light-darck-mode/idex";
+import RandomColor from "../random-color";
+import TictacToe from "../tic-tac-toe";
+import TreeView from "../tree-view";
+import { FeatureFlagContext } from "./context";
 
+export default function FeatureFlags() {
+  const { loading, enableFlags } = useContext(FeatureFlagContext);
 
+  const componentsToRender = [
+    {
+      key: "showLightAndDarkMode",
+      component: <LightDarkMode />,
+    },
+    {
+      key: "showTictacToeBoard",
+      component: <TictacToe />,
+    },
+    {
+      key: "showRandomColorGenerator",
+      component: <RandomColor />,
+    },
+    {
+      key: "showAccordian",
+      component: <Accordian />,
+    },
+    {
+      key: "shoTreeView",
+      component: <TreeView />,
+    },
+  ];
 
-export default function FeatureFlags(){
-    return <div>
-        <h1>Feature Flags</h1>
+  function checkEnableFlags(getCurrentKey) {
+    return enableFlags[getCurrentKey];
+  }
+
+  if (loading) return <h1>loading data</h1>;
+
+  return (
+    <div>
+      <h1>Feature Flags</h1>
+      {componentsToRender.map((componentItem) =>
+        checkEnableFlags(componentItem.key) ? componentItem.component : null
+      )}
     </div>
+  );
 }
